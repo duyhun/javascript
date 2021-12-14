7122867316c5083db9ce4a6b338af83b4c9d6948
duyhung <duyhung.pham.@gmail.com>
2021-12-14 11:12:43 +0700

    second commit

---
+++ Lession_2/index.js
@@ -0,0 +1,10 @@
+let inputR;
+let pi = 3.14;
+
+inputR = prompt("Nhập bán kính hình tròn");
+
+let C = 2*inputR*pi;
+let S = inputR^2*pi;
+
+document.write("Chu vi hình tròn là: " + C);
+document.write("Diện tích hình tròn là: " + S);
