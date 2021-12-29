481f2e854e49bd5d63289521f1bc4cf3bfbd9a6b
duyhung <duyhung.pham.@gmail.com>
2021-12-29 10:50:38 +0700

    tank

--- Tank/tank.js
+++ Tank/tank.js
@@ -1,10 +1,22 @@
 let vertical = 0;
 let horizontal = 0;
-function move_right() {
+function moveDown() {
     vertical += 19;
     document.getElementById("tank").style["top"] = vertical + "px";
+    
 }
-function move_down() {
+function moveUp() {
+    vertical -= 19;
+    document.getElementById("tank").style["top"] = vertical + "px";
+    
+}
+function moveRight() {
     horizontal += 38;
     document.getElementById("tank").style["left"] = horizontal + "px";
+    
+}
+function moveLeft(){
+    horizontal -= 38;
+    document.getElementById("tank").style["left"] = horizontal + "px";
+    
 }
